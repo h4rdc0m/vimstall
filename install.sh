61dc624f8d0c0dc75992dac40f36a4c33555fdc5
Christiaan van Dam <chris@trace.nl>
2025-02-26 14:09:34 +0000

    Rounding out the minimalistic config a bit

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo Installing NodeJS...
-sudo apt install -y nodejs
+sudo apt install -y nodejs npm
 
 echo Installing color scheme...
 mkdir -p ~/.vim/colors
@@ -9,8 +9,14 @@ curl -o molokai.vim https://raw.githubusercontent.com/tomasr/molokai/master/colo
 echo Installing Plug plugin manager...
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+mkdir -p ~/intelephense
+vi ~/intelephense/license.txt
+
 echo Copying our .vimrc
 cp .vimrc ~/.vimrc
+echo Copying coc-settings.json
+cp ./coc-settings.json ~/.vim/coc-settings.json
+
 vim -c 'PlugInstall|q'
 vim -c 'PlugUpdate|q'
-vim -c 'CocInstall -sync coc-json coc-html|q'
+vim -c 'CocInstall -sync coc-json coc-html @yaegassy/coc-intelephense|q'
